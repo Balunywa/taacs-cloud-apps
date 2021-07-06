30f1486bb9ee0cc96c0eeec781ac6154e55aa085
Lukman Balunywa <81341827+Balunywa@users.noreply.github.com>
2021-07-06 18:12:10 -0400

    uploading the new code to the repo
    uploading the new code to the repo

---
+++ src/main/java/taacs/cloud/sevs/LoginServlet.java
@@ -0,0 +1,27 @@
+package taacs.cloud.sevs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException 
+	{
+
+		String name = request.getParameter("name");
+        request.setAttribute("name", name);
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+
+}
